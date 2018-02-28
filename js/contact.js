8f9af8255f7abe6f99b5e14f41b7de0aae05828e
joshua-deans <joshua.deans21@gmail.com>
2018-02-27 21:52:55 -0800

    Form update

--- js/contact.js
+++
@@ -1,9 +0,0 @@
-$.post($("#contact-us-form").attr('action'), JSON.stringify({
-    name: $("#contact-us-form input[name='name']").val(),
-    email: $("#contact-us-form input[name='email']").val(),
-    message: $("#contact-us-form textarea[name='message']").val(),
-    'g-recaptcha-response': $("#contact-us-form textarea[name='g-recaptcha-response']").val()
-}), function (data) {
-    $(".thanks").show();
-    $("#contact-us-form button").hide();
-}, 'json');
