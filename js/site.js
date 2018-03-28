1b893c3ba32190ff79ed7d333131ef5097aa3e2d
joshua-deans <joshua.deans21@gmail.com>
2018-03-27 21:10:25 -0700

    Update w/ animations

--- js/site.js
+++ js/site.js
@@ -1,3 +1,23 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#self-image', {distance: '0 px', scale: 0.4, delay: 70});
+
+sr.reveal('#top-card', {scale: 1, distance: '100px', delay: 170});
+
+sr.reveal('#skills', {origin: 'left'});
+
+sr.reveal('#work-exp', {origin: 'left'});
+
+sr.reveal('#education', {origin: 'left'});
+
+sr.reveal('#volunteer-start', {origin: 'right'});
+
+sr.reveal('#projects', {origin: 'right'});
+
+sr.reveal('#interests', {origin: 'right'});
+
+sr.reveal('#contact-card');
+
 var progressBar = document.querySelector("#myBar");
 
 window.addEventListener("scroll", function(event){
